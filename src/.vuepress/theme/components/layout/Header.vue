3f83a38c2f2bcc4c4bf67e205c01bb1d7987f279
Axel Pardemann <axelitusdev@gmail.com>
2020-07-07 20:10:45 -0500

    Adjust styles

--- src/.vuepress/theme/components/layout/Header.vue
+++ src/.vuepress/theme/components/layout/Header.vue
@@ -7,11 +7,11 @@
         </div>
 
         <svg class="w-full flex-no-shrink -mb-px" style="height: 3vw" preserveAspectRatio="none" viewBox="0 0 2340 120">
-            <path fill="var(--color-neutral-100)" d="M0 76.353h2340v43H0z" />
+            <path fill="var(--color-neutral-200)" d="M0 76.353h2340v43H0z" />
             <path fill="var(--color-primary-900)" d="M2340 0v120L0 78.956z" />
         </svg>
         <!-- Fix Firefox quirk -->
-        <div class="h-px bg-neutral-100"></div>
+        <div class="h-px bg-neutral-200"></div>
     </header>
 </template>
 

61c31890dfeca461901a6cda56a77851fde62e58
Axel Pardemann <axelitusdev@gmail.com>
2020-07-18 20:46:06 -0500

    Align post card title and featured icon in post card

--- src/.vuepress/theme/components/posts/PostCard.vue
+++ src/.vuepress/theme/components/posts/PostCard.vue
@@ -1,15 +1,15 @@
 <template>
     <article class="post-card my-8 px-3 py-2 bg-neutral-050 rounded shadow-lg">
         <header class="relative mb-1">
-            <span class="absolute top-0 right-0" v-if="post.isFeatured">
-                <svg class="text-support-fifth-500 h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
+            <div class="flex items-center justify-between pb-1 border-b-4 border-neutral-300">
+                <h2 class="font-semibold text-xl xl:text-2xl ">
+                    <a v-html="post.renderedTitle" class="text-neutral-600 hover:text-primary-800" :href="post.path" />
+                </h2>
+                <svg v-if="post.isFeatured" class="text-support-fifth-500 h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
                     <title>Featured!</title>
                     <path fill-rule="evenodd" d="M11.757 2.034a1 1 0 01.638.519c.483.967.844 1.554 1.207 2.03.368.482.756.876 1.348 1.467A6.985 6.985 0 0117 11a7.002 7.002 0 01-14 0c0-1.79.684-3.583 2.05-4.95a1 1 0 011.707.707c0 1.12.07 1.973.398 2.654.18.374.461.74.945 1.067.116-1.061.328-2.354.614-3.58.225-.966.505-1.93.839-2.734.167-.403.356-.785.57-1.116.208-.322.476-.649.822-.88a1 1 0 01.812-.134zm.364 13.087A2.998 2.998 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879.586.585.879 1.353.879 2.121s-.293 1.536-.879 2.121z" clip-rule="evenodd" />
                 </svg>
-            </span>
-            <h2 class="border-b-4 border-neutral-300">
-                <a v-html="post.renderedTitle" class="font-semibold text-xl xl:text-2xl text-neutral-600 hover:text-primary-800" :href="post.path" />
-            </h2>
+            </div>
             <div class="flex flex-col md:flex-row justify-between mt-1 text-sm text-neutral-400">
                 <div>
                     <app-post-card-permalink :date="post.isoDate" :permalink="post.frontmatter.permalink" />
@@ -51,7 +51,3 @@
         }
     }
 </script>
-
-<style scoped>
-
-</style>

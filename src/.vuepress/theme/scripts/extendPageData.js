ec5517df847e0844b9f25f529b081a079243b2c9
Axel Pardemann <axelitusdev@gmail.com>
2020-07-19 19:31:47 -0500

    Remove extra spaces

--- src/.vuepress/theme/scripts/extendPageData.js
+++ src/.vuepress/theme/scripts/extendPageData.js
@@ -47,17 +47,17 @@ function renderTitle(page) {
 
 function setPublishedAt(page) {
     let date_regex = /(?<date>(?<year>\d{4})-?(?<month>0[0-9]|1[0-2])-?(?<day>[0-2][0-9]|3[0-1]))(?:[T| ](?<time>(?<hours>[0-1][0-9]|2[0-3]):?(?<minutes>[[0-5][0-9]):?(?<seconds>[0-5][0-9])(?<fraction>.000)?(?:(?<timezone>Z|[+-](?:0[0-9]|1[0-2]):00))))?/gi
-    
+
     if (page.frontmatter.published_at === undefined || page.frontmatter.published_at === false) {
         page.published_at = false
         return
     }
-    
+
     let matches = date_regex.exec(page.frontmatter.published_at.toISOString()).groups
     if (matches.time === '00:00:00.000Z') {
         page.published_at = new Date(moment.tz(matches.date + ' 09:00:00', 'America/Mexico_City').format())
         return
     }
-    
+
     page.published_at = page.frontmatter.published_at
 }

cd24eed205abdb9b1377e6053ddd27501e08a838
Axel Pardemann <axelitusdev@gmail.com>
2020-07-25 15:57:51 -0500

    Add RSS feed

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,7 @@
-const {description} = require('../../package')
+const description = require('../../package').description.replace("Axel Pardemann Blog - ", "")
 const twemoji = require('twemoji')
+
+const canonical_base = 'https://axel.pardemann.blog'
 
 module.exports = {
     title: "Axel Pardemann Blog",
@@ -16,7 +18,7 @@ module.exports = {
     ],
 
     themeConfig: {
-        url: 'https://axel.pardemann.blog',
+        url: canonical_base,
         cssTheme: 'theme-nord',
         nav: {
             title: 'Axel Pardemann',
@@ -30,9 +32,48 @@ module.exports = {
             ]
         }
     },
-    
+
     plugins: [
         '@vuepress/last-updated',
+        ['vuepress-plugin-feed', {
+            canonical_base: canonical_base,
+            description: description,
+            posts_directories: ['/posts/'],
+            feedLinks: {
+                rss: `${canonical_base}/rss.xml`,
+                json: `${canonical_base}/feed.json`,
+                atom: `${canonical_base}/feed.atom`,
+            },
+            feeds: {
+                rss2: {
+                    enable: true,
+                    filename: 'rss.xml',
+                    head_link: {
+                        enable: true,
+                        type: 'application/rss+xml',
+                        title: '%%site_title%% RSS Feed',
+                    },
+                },
+                atom1: {
+                    enable: true,
+                    file_name: 'feed.atom',
+                    head_link: {
+                        enable: true,
+                        type: 'application/atom+xml',
+                        title: '%%site_title%% Atom Feed',
+                    },
+                },
+                json1: {
+                    enable: true,
+                    file_name: 'feed.json',
+                    head_link: {
+                        enable: true,
+                        type: 'application/json',
+                        title: '%%site_title%% JSON Feed',
+                    },
+                }
+            }
+        }],
     ],
 
     postcss: {
